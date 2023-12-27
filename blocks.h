1df9603e9f353c55d8bd240baca03e4d17f38fcb
limnwiki <limnwiki@proton.me>
2023-12-27 14:48:32 -0800

    + update

--- blocks.h
+++ blocks.h
@@ -3,8 +3,8 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
     {"|", "", 0 ,0},
     {"💽 ", "diskusage", 60, 0},
-	{"📲 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 5, 0},
-	{"📅 ", "date '+%b %d (%a)'", 5, 0},
+	{"🚨 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 5, 0},
+	{"📅 ", "date '+%Y %b %d (%a)'", 5, 0},
 	{"🕗 ", "date '+%I:%M:%S%p'", 1, 0},
     {"🌞 ", "daypercent", 10, 0},
     {"🌚 ", "daypercent -r", 10, 0},
